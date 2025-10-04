f0babfbae813853281f97c4e7e0b9dcb422a27e7
Cheseed Sharon Bangura <cheseedsharonbangura@Cheseeds-MacBook-Pro.local>
2025-10-04 16:15:32 +0000

    complete chatbot

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, jsonify, request
 from src.helper import download_hugging_face_embeddings
 from langchain_pinecone import PineconeVectorStore
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_openai import ChatOpenAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
@@ -36,35 +37,29 @@ docsearch = PineconeVectorStore.from_existing_index(
 
 retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
 
-chatModel = ChatOpenAI(model="gpt-4o")
+# chatModel = ChatOpenAI(model="gpt-4o")
+chatModel = ChatGoogleGenerativeAI(model="models/gemini-1.5-pro-latest")
+
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
         ("human", "{input}"),
-    ]
+    ] 
 )
 
 question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
+@app.route("/hello", methods=["GET"])
+def hello():
+    return jsonify({"answer": "Hello, Jogoh!"})
 
-
-@app.route("/")
-def index():
-    return render_template('chat.html')
-
-
-
-@app.route("/get", methods=["GET", "POST"])
+@app.route("/get", methods=["POST"])
 def chat():
-    msg = request.form["msg"]
-    input = msg
-    print(input)
+    msg = request.json.get("msg")  # Expect JSON instead of form-data
     response = rag_chain.invoke({"input": msg})
-    print("Response : ", response["answer"])
-    return str(response["answer"])
-
+    return jsonify({"answer": response["answer"]})
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+    app.run(host="0.0.0.0", port= 8000, debug= True)
